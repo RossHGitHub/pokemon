f3c07044d1e41ff32f09f9f892426592d6184df0
Ross Henderson <rossliamhenderson@gmail.com>
2023-06-15 00:20:07 +0100

    Finished basic styling.. Aaand basic API and display functions.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+import { getData } from './apiFunctions';
+import { processData } from './apiFunctions';
+
+
+const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById('searchInput');
+
+
+searchButton.addEventListener('click', ()=>{processData(searchInput.value);});
